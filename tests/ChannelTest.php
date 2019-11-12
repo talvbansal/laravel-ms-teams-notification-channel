20a35251d2f287de101f619c7a1cba50105fbb92
Talv <talvbansal@hotmail.com>
2019-11-12 22:28:24 +0000

    Apply fixes from StyleCI

--- tests/ChannelTest.php
+++ tests/ChannelTest.php
@@ -1,15 +1,13 @@
 <?php
 
-
 namespace NotificationChannels\MsTeams\Test;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Notifications\Notification;
-use Mockery;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Response;
 use NotificationChannels\MsTeams\Exceptions\CouldNotSendNotification;
 use NotificationChannels\MsTeams\MsTeamsChannel;
 use NotificationChannels\MsTeams\MsTeamsMessage;
@@ -37,7 +35,8 @@ class ChannelTest extends TestCase
     /**
      * @test
      */
-    public function it_throws_an_exception_when_it_could_not_send_the_notification(){
+    public function it_throws_an_exception_when_it_could_not_send_the_notification()
+    {
         $this->expectException(CouldNotSendNotification::class);
 
         $channel = new MsTeamsChannel(new Client());
@@ -48,6 +47,7 @@ class ChannelTest extends TestCase
 class TestNotifiable
 {
     use Notifiable;
+
     /**
      * @return int
      */
